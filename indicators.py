a232485a66e5f3863298361de348dd667deb6bc6
spirlo97 <samuel.pirlo@rabobank.nl>
2024-02-16 16:42:39 +0100

    updated indicator code.

--- indicators.py
+++ indicators.py
@@ -3,26 +3,31 @@ import numpy as np
 from settings import input_path, country_map
 from forex_python.converter import CurrencyRates
 from datetime import datetime
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
+from main import DataProcessor
+from api_sourcing.main import json_to_df
 
 
-def min_max_scaling(x, min_x, max_x):
-    # #NOTE: How to treat GVA's that are zero?
-    # x.replace([np.inf, -np.inf], np.nan, inplace=True)
-    #NOTE: How to treat missing values
+def min_max_scaling(x):
+    # Compute min and max values of x.
+    min_x=data['indOperationCostsMortgages'].min(skipna=True)
+    max_x=data['indOperationCostsMortgages'].max(skipna=True)
+
+    #NOTE: How to treat GVA's that are zero?
+    # Replace (negative) infinite values with NaN entries.
+    x.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+    #NOTE: How to treat missing values.
+    # Replace empty values with mean of non empty values.
     x = x.fillna(x.mean(skipna=True))
 
+    # Apply scaling.
     x = (x - min_x) / (max_x - min_x)
 
     return x
 
-scaler = MinMaxScaler()
-
 # GVA 
 df_gva = pd.read_excel(input_path / 'National Accounts.xlsx', sheet_name='VA_CP')
 df_gva = df_gva.drop(columns=['var'])
-data = df_gva[['geo_code', 'nace_r2_name', '2018']].rename(columns={'2018': 'GVA'})
 
 # >>>>> Proxy indicators <<<<<
 
@@ -34,41 +39,42 @@ df_h_spending = pd.read_csv(input_path / 'H_SPENDING.csv').rename(columns={
     'LOCATION': 'geo_code', 'Value': 'H_Spending'})
 df_h_spending.geo_code = df_h_spending.geo_code.map(country_map)
 
+data = df_gva[['geo_code', 'nace_r2_name', '2018']].rename(columns={'2018': 'GVA'})
+
 data = data.merge(df_h_income[['geo_code', 'H_Income']], on='geo_code', how='left')
 data = data.merge(df_h_spending[['geo_code', 'H_Spending']], on='geo_code', how='left')
 
-data['OperationCostsMortgages'] = (data['H_Spending'] / data['H_Income'])
-data['OperationCostsMortgages'].replace([np.inf, -np.inf], np.nan, inplace=True)
+data['indOperationCostsMortgages'] = ((data['H_Spending'] / data['H_Income']) / data['GVA'])
+data['indOperationCostsMortgages'].replace([np.inf, -np.inf], np.nan, inplace=True)
 
-upper_abs = np.nanpercentile(data['OperationCostsMortgages'], q=90)
-lower_abs = np.nanpercentile(data['OperationCostsMortgages'], q=10)
+data['indOperationCostsMortgages'] = min_max_scaling(x=data['indOperationCostsMortgages'])
 
-data['OperationCostsMortgages'] = np.clip(data['OperationCostsMortgages'],
-                                          a_min=lower_abs, 
-                                          a_max=upper_abs)
+# - Water abstraction
+params = {
+    "format": "format=JSON",
+    "filters": {"time":"2019"},
+    "sinceTimePeriod" : "sinceTimePeriod = 2012",
+    "unit":"unit=MIO_M3"
+}
+dataset = "env_wat_abs"
+d = DataProcessor(dataset=dataset, params=params)
+df, meta_data = d.process_data()
 
-data['indOperationCostsMortgages'] = min_max_scaling(
-    x=data['OperationCostsMortgages'],
-    min_x=data['OperationCostsMortgages'].min(skipna=True),
-    max_x=data['OperationCostsMortgages'].max(skipna=True))
+df_water_abstraction = df
 
-data['indOperationCostsMortgages2'] = scaler.fit_transform(data['OperationCostsMortgages'].values.reshape(-1, 1))
+data = data.merge(df_water_abstraction[['geo_code', 'Water_Abstraction']], on='geo_code', how='left')
 
-
-print(data.groupby('nace_r2_name')['indOperationCostsMortgages2'].mean())
+data['indWaterAbstraction'] = min_max_scaling(data['Water_Abstraction'] / data['GVA'])
 
 
-# - Transport and supply
-df_Ip_TraEq = pd.read_csv(input_path / 'old_proxies' / 'capital accounts.csv')
-df_Ip_TraEq = df_Ip_TraEq[['geo_code', 'nace_r2_name', 'Ip_TraEq']]
 
-data = data.merge(df_Ip_TraEq, on=['nace_r2_name', 'geo_code'], how='left')
-data['TransportSupply'] = data['Ip_TraEq'] / data['GVA']
-data['indTransportSupply'] = data.groupby('nace_r2_name')['TransportSupply'].transform('mean')
 
-data['indTransportSupply'] = min_max_scaling(
-    x=data['indTransportSupply'], 
-    min_x=data['indTransportSupply'].min(skipna=True),
-    max_x=data['indTransportSupply'].max(skipna=True))
 
-data['indTransportSupply'] = scaler.fit_transform(data['indTransportSupply'].values.reshape(-1, 1))
+
+
+
+
+
+
+
+
