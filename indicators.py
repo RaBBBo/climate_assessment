b1a2c46975a68e23c520344dc0fa469668c18a54
Xinyi Wang <xinyi.wang@rabobank.nl>
2024-02-23 14:21:54 +0100

    add transport ind

--- indicators.py
+++ indicators.py
@@ -44,7 +44,7 @@ data = df_gva[['geo_code', 'nace_r2_name', '2018']].rename(columns={'2018': 'GVA
 data = data.merge(df_h_income[['geo_code', 'H_Income']], on='geo_code', how='left')
 data = data.merge(df_h_spending[['geo_code', 'H_Spending']], on='geo_code', how='left')
 
-data['indOperationCostsMortgages'] = ((data['H_Spending'] / data['H_Income']) / data['GVA'])
+data['indOperationCostsMortgages'] = (data['H_Spending'] / data['H_Income'])
 data['indOperationCostsMortgages'].replace([np.inf, -np.inf], np.nan, inplace=True)
 
 data['indOperationCostsMortgages'] = min_max_scaling(x=data['indOperationCostsMortgages'])
@@ -67,6 +67,20 @@ data = data.merge(df_water_abstraction[['geo_code', 'Water_Abstraction']], on='g
 data['indWaterAbstraction'] = min_max_scaling(data['Water_Abstraction'] / data['GVA'])
 
 
+# - Transport and supply
+df_Ip_TraEq = pd.read_csv(input_path / 'old_proxies' / 'capital accounts.csv')
+df_Ip_TraEq = df_Ip_TraEq[['geo_code', 'nace_r2_name', 'Ip_TraEq']]
+
+data = data.merge(df_Ip_TraEq, on=['nace_r2_name', 'geo_code'], how='left')
+data['TransportSupply'] = data['Ip_TraEq'] / data['GVA']
+data['indTransportSupply'] = data.groupby('nace_r2_name')['TransportSupply'].transform('mean')
+
+data['indTransportSupply'] = min_max_scaling(
+    x=data['indTransportSupply'],
+    min_x=data['indTransportSupply'].min(skipna=True),
+    max_x=data['indTransportSupply'].max(skipna=True))
+
+data['indTransportSupply'] = scaler.fit_transform(data['indTransportSupply'].values.reshape(-1, 1))
 
 
 
