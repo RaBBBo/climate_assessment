6da6a8c00557cb74849d3035978a274f2555bb96
xinyixinyiwang <160229666+xinyixinyiwang@users.noreply.github.com>
2024-03-11 10:57:26 +0100

    Update api_connect.py

--- api_sourcing/api_connect.py
+++ api_sourcing/api_connect.py
@@ -1,21 +1,26 @@
 import requests
 import pandas
 import json
-
+import typing
 
 class APIClient:
     def __init__(self):
         self.host_url = "https://ec.europa.eu/eurostat/api/dissemination/"
 
-    def get_data(self, dataset, params):
-        if params:
-            url = f"{self.host_url}statistics/1.0/data/{dataset}?{params['format']}&{params['sinceTimePeriod']}&lang=EN&{params['filters']}"
-        else:
-            url = f"{self.host_url}statistics/1.0/data/{dataset}?lang=EN"
-
-        # url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/env_wat_abs?format=JSON&sinceTimePeriod=2012&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=EL&unit=MIO_M3&wat_proc=ABST&wat_src=FSW&lang=en"
-        url = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/env_wat_abs?format=JSON&sinceTimePeriod=2015&geo=BE&geo=BG&geo=CZ&unit=MIO_M3&wat_proc=ABS_AGR&wat_proc=ABS_IND&wat_src=FSW&lang=en"
-        response = requests.get(url)
-        return response.json()
-
-
+    def get_data(self, dataset, params=None):
+        try:
+            base_url = f"{self.host_url}statistics/1.0/data/{dataset}?format=JSON"
+            param_str = ""
+            for param, val in params.items():
+                if isinstance(val, list):
+                    param_lst = "&".join([f"{param}={v}" for v in val])
+                elif isinstance(val, str):
+                    param_lst = f"{param}={val}"
+                    
+                param_str += f"&{param_lst}"
+                
+            url = f"{base_url}{param_str}&lang=en"
+            response = requests.get(url)
+            return response.json()
+        except:
+            print(f"request not valid adjust params.")
